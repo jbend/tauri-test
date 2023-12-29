d475333bf05144191789a428300ae2c6a2fe5c4e
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2023-12-28 19:53:13 -0800

    Added shadcn-svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,9 @@
   import Inventory from './components/svg/Inventory.svelte';
   import RequestQuote from './components/svg/RequestQuote.svelte';
   import Settings from './components/svg/Settings.svelte';
+  import BasicTable from './components/BasicTable.svelte';
+
+  import { Button } from './components/ui/button';
   
   const recentVendors = [
     {
@@ -36,11 +39,34 @@
       <Settings size="36" />
     </div>
   </aside>
-  <main class="grow flex">
-    <div class="">
-      <p>
-        Click on the Tauri, Vite, and Svelte logos to learn more.
-      </p>
+  <main class="grow p-4">
+    <div class="h-full overflow-hidden box-border bg-base">
+      <div class="max-h-full overflow-y-auto">
+
+        <p class="m-2 p-4">
+          Click on the Tauri, Vite, and Svelte logos to learn more.
+        </p>
+        
+        <div class="py-12 flex flex-col items-center gap-2">
+          <p>There are no order requests</p>
+          <RequestQuote size="96" />
+        </div>
+        
+        <div class="py-4 flex justify-around gap-2">
+          <Button variant="default">Default</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="link">Link</Button>
+          <Button variant="destructive">Destructive</Button>
+          <Button variant="outline">Outline</Button>
+          <Button variant="ghost">Ghost</Button>
+        </div>
+        
+        <div class="py-12 flex flex-col items-center gap-2">
+          <BasicTable />
+        </div>
+        
+      </div>
+
     </div>
   </main>
 </div>
